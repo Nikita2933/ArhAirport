8a7d3ebed9cbf7816994b813281ef9366fcc26a4
Nikita <nikita.ivanov@btc-s.ru>
2021-10-18 10:45:23 +0300

    done??

--- ArhAirport/ModelView/Departure/PageDepartureModelView.swift
+++ ArhAirport/ModelView/Departure/PageDepartureModelView.swift
@@ -20,32 +20,29 @@ final class PageDepartureModelView {
     func createArrForPageDeparture(times: DayTimePage, test: DepartureData?, closure: @escaping DepartureCallback) {
         let downloadOperation = DownloadOperations(times: times, typeAirline: .departure)
         let dataBaseOperation = DataBaseOperations(type: .departure, old: test)
-
-        let secondOperations = BlockOperation {
+        dataBaseOperation.addDependency(downloadOperation)
+        
+        let operationQueue = OperationQueue()
+    
+        operationQueue.addOperation(downloadOperation)
+        
+        operationQueue.addBarrierBlock {
             if let result = downloadOperation.result {
                 switch result {
                 case .failure(let error):
                     print(error)
                     dataBaseOperation.cancel()
                 case .success(let departureModel):
+                    dataBaseOperation.model = departureModel
                     DispatchQueue.main.async {
-                        dataBaseOperation.model = departureModel
                         dataBaseOperation.start()
                         if let data = dataBaseOperation.outputObject as? DepartureData {
                             closure(data)
                         }
                     }
                 }
-
             }
         }
-        
-        
-        secondOperations.addDependency(downloadOperation)
-
-        let operationQueue = OperationQueue()
-        
-        operationQueue.maxConcurrentOperationCount = 1
-        operationQueue.addOperations([downloadOperation, secondOperations], waitUntilFinished: true)
     }
 }
+
